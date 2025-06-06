536b2745833f68ee97a0b21375a7e4ccba002adf
ShimuGuyue <2147539031@qq.com>
2025-06-06 17:19:58 +0800

    update：SupermarketManagementSystem
    代码重构，将菜单功能内置到Supermarket类中

--- SupermarketManagementSystem/src/huyansong/Supermarket.java
+++ SupermarketManagementSystem/src/huyansong/Supermarket.java
@@ -16,12 +16,90 @@ public class Supermarket {
         goods = new java.util.HashMap<>();
     }
 
+    /*  菜单功能
+     * 用户输入参数，系统调用方法。
+     * */
+    public void Menu() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("正在进入超市管理系统...");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("超市管理系统进入成功！");
+        System.out.println("键入Enter键继续...");
+        sc.nextLine();
+
+        while (true){
+            // 用户输入操作类型，系统执行对应操作
+            System.out.println("系统菜单如下：");
+            System.out.println("1)销售功能      2)添加功能      3)查询功能      4)修改功能");
+            System.out.println("5)删除功能      6)统计功能      7)信息存盘      8)信息读取");
+            System.out.println("q)退出系统");
+            System.out.println("请输入要执行的操作：");
+            char mode = sc.nextLine().charAt(0);
+
+            // 判断何时结束循环，退出系统
+            boolean work = true;
+
+            switch (mode) {
+                case '1':
+                    this.Sale_good(sc);
+                    break;
+                case '2':
+                    this.Add_good(sc);
+                    break;
+                case '3':
+                    this.Find_good(sc);
+                    break;
+                case '4':
+                    this.Modify_good(sc);
+                    break;
+                case '5':
+                    this.Delete_good(sc);
+                    break;
+                case '6':
+                    this.Collect_goods();
+                    break;
+                case '7':
+                    this.Save_data();
+                    break;
+                case '8':
+                    this.Load_data();
+                    break;
+                case 'q':
+                    work = false;
+                    break;
+                default:
+                    System.out.println("输入非法！请重新输入！");
+            }
+            // 当用户按退出键时退出管理系统
+            if (!work)
+            {
+                System.out.println("正在退出超市管理系统...");
+                System.out.println("*");
+                System.out.println("*");
+                System.out.println("*");
+                System.out.println("超市管理系统退出成功！");
+                break;
+            }
+
+            // 等待用户键入Enter键再继续下一次循环，为用户留出反应时间
+            System.out.println("操作完成，请键入Enter键继续...");
+            sc.nextLine();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+        }
+
+        sc.close();
+    }
+
     /*  销售功能
      * 购买商品时，先输入类别，然后输入商品名称，并在库存中查找该商品的相关信息。
      * 如果有库存量，输入购买的数量，进行相应计算。
      * 如果库存量不够，给出提示信息，结束购买。
      * */
-    public void Sale_good(Scanner sc){
+    private void Sale_good(Scanner sc){
         // 按类别查找商品
         System.out.println("请输入您要购买的商品类别：");
         String type = sc.nextLine();
@@ -63,7 +141,7 @@ public class Supermarket {
     /*  添加功能
      * 主要完成商品信息的添加。
      * */
-    public void Add_good(Scanner sc){
+    private void Add_good(Scanner sc){
         System.out.println("请输入商品类别：");
         String type = sc.nextLine();
         System.out.println("请输入商品名称：");
@@ -85,11 +163,11 @@ public class Supermarket {
 
     /*  查询功能
      * 可按商品类别、商品名称、生产厂家进行查询。
-     * 若存在相应信息，输出所查询的信息，
+     * 若存在相应信息，输出所查询的信息；
      * 若不存在该记录，则提示“该记录不存在！”。
      * */
-    // public 函数用于用户调用
-    public void Find_good(Scanner sc){
+    // 方法变种1：用于用户查找物品
+    private void Find_good(Scanner sc){
         System.out.println("请输入您要查询的商品类别：");
         String type = sc.nextLine();
         System.out.println("请输入您要查询的商品名称：");
@@ -125,7 +203,7 @@ public class Supermarket {
         System.out.println("[生产厂家：" + good.manufacturer + "]");
         System.out.println("[品牌：" + good.brand + "]");
     }
-    // private 函数用于其他函数调用
+    // 方法变种2：用于其他函数调用
     private Good Find_good(String type, String name) {
 
         // 判断是否存在查询的商品类别
@@ -145,9 +223,9 @@ public class Supermarket {
     }
 
     /*  修改功能
-     * 可根据查询结果对相应的记录进行修改。
+     * 根据查询结果对相应的记录进行修改。
      * */
-    public void Modify_good(Scanner sc) {
+    private void Modify_good(Scanner sc) {
         System.out.println("请输入您要修改的商品类别：");
         String type0 = sc.nextLine();
         System.out.println("请输入您要修改的商品名称：");
@@ -184,10 +262,10 @@ public class Supermarket {
 
     /*  删除功能
      * 主要完成商品信息的删除。
-     * 先输入商品类别，再输入要删除的商品名称，根据查询结果删除该物品的记录，
+     * 先输入商品类别，再输入要删除的商品名称，根据查询结果删除该物品的记录。
      * 如果该商品不在物品库中，则提示“该商品不存在”。
      * */
-    public void Delete_good(Scanner sc){
+    private void Delete_good(Scanner sc){
         System.out.println("请输入您要删除的商品类别：");
         String type = sc.nextLine();
         System.out.println("请输入您要删除的商品名称：");
@@ -210,7 +288,7 @@ public class Supermarket {
     /*  统计功能
      * 收集所有商品信息，按库存量从高到低排序，并输出结果。
      * */
-    public void Collect_goods(){
+    private void Collect_goods(){
         System.out.println("正在整理商品信息...");
         System.out.println("*");
         System.out.println("*");
@@ -245,7 +323,7 @@ public class Supermarket {
     /*  导出信息
      * 将商品信息写入文件。
      * */
-    public void Save_data() throws IOException {
+    private void Save_data() throws IOException {
         System.out.println("正在导出商品信息...");
         System.out.println("*");
         System.out.println("*");
@@ -266,7 +344,7 @@ public class Supermarket {
     /*  读入信息
      * 从文件中将商品信息读入程序。
      * */
-    public void Load_data() throws IOException {
+    private void Load_data() throws IOException {
         goods.clear();
 
         System.out.println("正在导入商品信息...");
