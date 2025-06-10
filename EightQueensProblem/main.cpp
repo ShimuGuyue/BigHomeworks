a4395125ddc5db3eacae5334da7529d5b944e710
ShimuGuyue <2147539031@qq.com>
2025-06-10 12:00:12 +0800

    upadte：EightQueensProblem
    重构项目文件结构

--- EightQueensProblem/main.cpp
+++ EightQueensProblem/main.cpp
@@ -1,10 +1,16 @@
+#include "src/EightQueens.h"
+
 #include <iostream>
 using std::cin;
 using std::cout;
 using std::endl;
+#include <windows.h>
 
 int main()
 {
+	// 将控制台输出编码设置为 UTF-8，使得中文能正确输出
+	SetConsoleOutputCP(CP_UTF8);
+	// 将 C++ 与 C 的输入输出流解除同步
 	std::ios::sync_with_stdio(false);
 
 
