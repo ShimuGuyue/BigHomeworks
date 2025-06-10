ab7144e577e0c28beedbcde7a41d4334e5d4f4f0
ShimuGuyue <2147539031@qq.com>
2025-06-10 13:02:29 +0800

    updata：EightQueensProblem
    重构 src\EightQueens.cpp 中算法演示处的代码结构

--- EightQueensProblem/src/EightQueens.cpp
+++ EightQueensProblem/src/EightQueens.cpp
@@ -1,7 +1,6 @@
 #include "EightQueens.h"
 
 #include <iostream>
-using namespace std;
 using std::cin;
 using std::cout;
 using std::endl;
@@ -9,6 +8,8 @@ using std::endl;
 using std::vector;
 #include <string>
 using std::string;
+using std::getline;
+#define LINE_IGNORE {string rubbish; getline(cin, rubbish);}
 
 EightQueens::EightQueens() : size(8), grid(size, vector<bool>(size, false))
 {}
@@ -17,8 +18,9 @@ void EightQueens::Menu()
 {
 	cout << "开始进行八皇后问题的求解算法演示..." << endl;
 	cout << "[键入 Enter 键继续]" << endl;
-	{string rubbish; getline(cin, rubbish);}
+	LINE_IGNORE
 
+	// 不断读取用户输入，进行算法演示
 	while (1)
 	{
 		cout << "请选择要演示的算法：" << endl;
@@ -30,64 +32,56 @@ void EightQueens::Menu()
 		string choice;
 		getline(cin, choice);
 		if (choice == "1")
-		{
-			cout << "开始演示递归回溯算法..." << endl;
 			Recursive_backtracking();
-		}
 		else if (choice == "2")
-		{
-			cout << "开始演示非递归回溯算法..." << endl;
 			NonRecursive_backtracking();
-		}
 		else if (choice == "3")
-		{
-			cout << "开始演示排列树的递归回溯算法..." << endl;
 			RecursiveTree_backtracking();
-		}
 		else if (choice == "q")
-		{
-			cout << "正在退出演示..." << endl;
-			cout << "程序已退出。" << endl;
 			break;
-		}
 		else
-		{
 			cout << "无效的选择，请重新输入。" << endl;
-		}
 
 		cout << "[键入 Enter 键继续]" << endl;
-		{string rubbish; getline(cin, rubbish);}
+		LINE_IGNORE
 	}
+	cout << "程序已退出。" << endl;
 }
 
 void EightQueens::Restore()
 {
-	for (int i = 0; i < size; ++i)
-	{
-		for (int j = 0; j < size; ++j)
-		{
-			grid[i][j] = false;
-		}
-	}
+	grid.assign(size, vector<bool>(size, false));
 }
 
 void EightQueens::Recursive_backtracking()
 {
+	cout << "开始演示递归回溯算法..." << endl;
 
 
+
+	// 算法演示完成后恢复棋盘
 	this->Restore();
+	cout << "递归回溯算法演示完成。" << endl;
 }
 
 void EightQueens::NonRecursive_backtracking()
 {
+	cout << "开始演示非递归回溯算法..." << endl;
 
 
+
+	// 算法演示完成后恢复棋盘
 	this->Restore();
+	cout << "非递归回溯算法演示完成。" << endl;
 }
 
 void EightQueens::RecursiveTree_backtracking()
 {
+	cout << "开始演示排列树的递归回溯算法..." << endl;
 
 
+
+	// 算法演示完成后恢复棋盘
 	this->Restore();
+	cout << "排列树的递归回溯算法演示完成。" << endl;
 }
